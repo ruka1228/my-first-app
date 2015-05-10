fd3949735da4179609fccde75abb1f6fac8dcde3
Wataru MIYAGUNI <gonngo@gmail.com>
2015-05-10 16:03:57 +0900

    モデルの作成

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,7 +1,15 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import play.db.ebean.Model;
 
+@Entity
 public class Task extends Model {
 
+    @Id
+    public Integer id;
+
+    public String name;
 }
