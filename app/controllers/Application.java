004be36a80dc32e557c7830e8f272e576f6383ae
Wataru MIYAGUNI <gonngo@gmail.com>
2015-05-10 16:30:00 +0900

    Chapter3 演習2 の準備

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,12 @@
 package controllers;
 
 import models.Task;
+
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import play.*;
 import play.mvc.*;
@@ -17,8 +19,11 @@ public class Application extends Controller {
     }
 
     public static Result tasks() {
+        // java.util.Random;
+        Random rnd = new Random();
+
         Task task   = new Task();
-        task.name   = "Pizza を食べる";
+        task.name   = "ピザを" + rnd.nextInt(10) + "枚食べる";
         task.period = new Date();
         task.save();
 
